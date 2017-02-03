cb4f89d252029072276ebc2ce01e884e39a8863b
Patrick Mueller <pmuellr@nodesource.com>
2017-02-03 12:04:40 -0500

    update to use 2.1.2 storage and console
    * updated manifests to use nsolid buildpack via git instead of bundled

--- nsolid-console-app/setup.sh
+++ nsolid-console-app/setup.sh
@@ -4,10 +4,10 @@
 PRODUCT=console
 
 # bundle version
-BUNDLE=2.1.0
+BUNDLE=2.1.2
 
 # version of the product
-VERSION=2.1.1
+VERSION=2.1.2
 
 # name of the tarball that will be uploaded to Cloud Foundry via cf push
 TARBALL=nsolid-$PRODUCT-linux-x64.tar.gz
--- nsolid-storage-app/setup.sh
+++ nsolid-storage-app/setup.sh
@@ -4,10 +4,10 @@
 PRODUCT=storage
 
 # bundle version
-BUNDLE=2.1.0
+BUNDLE=2.1.2
 
 # version of the product
-VERSION=1.1.1
+VERSION=1.2.2
 
 # name of the tarball that will be uploaded to Cloud Foundry via cf push
 TARBALL=nsolid-$PRODUCT-linux-x64.tar.gz
