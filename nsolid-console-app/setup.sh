ec064fc3397161f9c4a815259daa90572e9d5bcb
Patrick Mueller <pmuellr@nodesource.com>
2017-06-08 07:34:26 -0400

    update to use 2.2.0 storage and console

--- nsolid-console-app/setup.sh
+++ nsolid-console-app/setup.sh
@@ -4,10 +4,10 @@
 PRODUCT=console
 
 # bundle version
-BUNDLE=2.1.4
+BUNDLE=2.2.0
 
 # version of the product
-VERSION=2.1.5
+VERSION=2.2.0
 
 # name of the tarball that will be uploaded to Cloud Foundry via cf push
 TARBALL=nsolid-$PRODUCT-linux-x64.tar.gz
--- nsolid-storage-app/setup.sh
+++ nsolid-storage-app/setup.sh
@@ -4,10 +4,10 @@
 PRODUCT=storage
 
 # bundle version
-BUNDLE=2.1.4
+BUNDLE=2.2.0
 
 # version of the product
-VERSION=1.2.3
+VERSION=1.4.0
 
 # name of the tarball that will be uploaded to Cloud Foundry via cf push
 TARBALL=nsolid-$PRODUCT-linux-x64.tar.gz
